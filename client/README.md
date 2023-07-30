5f536d04d82379e474d4a3a425bee12c54a32439
Dusuna <dusunax@gmail.com>
2023-07-31 00:18:35 +0900

    init: socket init for server & client

---
+++ client/README.md
@@ -0,0 +1,12 @@
+# three-socket-client
+
+3D rendering with webSocket!
+
+- three.js
+
+- simple-peer
+
+```
+npm install simple-peer
+npm install --save @types/simple-peer
+```
---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+const http = require("http");
+
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+const port = 3001;
+
+// 새로운 클라이언트가 접속했을 때
+io.on("connection", (socket) => {
+  console.log("새로운 클라이언트가 접속했습니다. ID:", socket.id);
+
+  // 동영상 스트림
+  // socket.on("stream", (stream) => {
+  //   socket.broadcast.emit("stream", stream);
+  // });
+
+  // 클라이언트가 메시지를 보냈을 때
+  socket.on("message", (data) => {
+    console.log("클라이언트가 메시지를 보냈습니다:", data);
+
+    // 모든 클라이언트에게 메시지 전송
+    io.emit("message", data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+});
